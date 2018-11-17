60309396f3549a581768402e3f6339c3b3411cf9
gstraube <gstraube@mailbox.org>
2018-11-17 17:00:32 +0100

    Perform initial handshake

--- app/src/main/java/com/github/librevnc/MainActivity.kt
+++ app/src/main/java/com/github/librevnc/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.github.librevnc
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import org.jetbrains.anko.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.lang.Exception
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        downloadData()
+    }
+
+    private fun downloadData() {
+        doAsync {
+            val vncClient = VncClient("192.168.42.64", 5900)
+
+            val wasHandshakeSuccessful = vncClient.performHandshake()
+
+            uiThread { /*Update the UI thread here*/ toast("Handshake successful: $wasHandshakeSuccessful") }
+        }
     }
 }
---
+++ app/src/main/java/com/github/librevnc/VncClient.kt
@@ -0,0 +1,50 @@
+package com.github.librevnc
+
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.warn
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.Socket
+
+class VncClient(host: String, port: Int) : AnkoLogger {
+
+    var socket: Socket = Socket(host, port)
+
+    fun performHandshake(): Boolean {
+        if (socket.isConnected) {
+            val inputStream = socket.getInputStream()
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val out = PrintWriter(socket.getOutputStream(), true)
+
+            val serverProtocolVersion = bufferedReader.readLine()
+            val supportedProtocolVersion = "RFB 003.008"
+            if (supportedProtocolVersion == serverProtocolVersion) {
+                out.write("$supportedProtocolVersion\n")
+                out.flush()
+            } else {
+                warn("Unsupported server protocol version: $serverProtocolVersion")
+
+                return false
+            }
+
+            val serverSecurityType = ByteArray(2)
+            inputStream.read(serverSecurityType)
+            val supportedSecurityType = 1
+            if (supportedSecurityType == serverSecurityType[1].toInt()) {
+                out.write(supportedSecurityType)
+                out.flush()
+            } else {
+                warn("Unsupported server security type: $serverSecurityType")
+
+                return false
+            }
+
+            return true
+        } else {
+            warn("Socket is not connected")
+
+            return false
+        }
+    }
+}
